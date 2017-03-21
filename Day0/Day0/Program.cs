6f27878eb35a154496d2cc9bbeb3fe57f3076a00
R1dge87 <ztstandridge@mail.usf.edu>
2017-03-20 21:14:21 -0400

    Add files via upload

---
+++ Day0/Day0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Hello, World");
+
+            Console.WriteLine(inputString);
+
+            Console.ReadLine();
+        }
+    }
+}

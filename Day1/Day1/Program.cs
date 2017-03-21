da4614ff179c9fe8cf1eb3df59a59eb1cd49e001
R1dge87 <ztstandridge@mail.usf.edu>
2017-03-20 21:14:56 -0400

    Add files via upload

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Day 1: Data Types
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank";
+
+            int j = int.Parse(Console.ReadLine());
+            double f = Convert.ToDouble(Console.ReadLine());
+            string t = Console.ReadLine();
+
+            Console.WriteLine(i + j);
+            Console.WriteLine((d + f).ToString("F1"));
+            Console.WriteLine(s + t);
+
+            Console.ReadLine();
+        }
+    }
+}

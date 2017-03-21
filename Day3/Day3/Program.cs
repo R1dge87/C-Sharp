f4f7e4a8cdfff434ef5b7f3ef2ab739d694b4ee7
R1dge87 <ztstandridge@mail.usf.edu>
2017-03-20 21:16:07 -0400

    Add files via upload

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intro into conditional statements
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n%2 > 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (n%2 == 0)
+            {
+                if (n >= 2 && n <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (n >= 6 && n <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

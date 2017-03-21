0cf45a009383f6d737b1bbb4f033e86ed811a2cc
R1dge87 <ztstandridge@mail.usf.edu>
2017-03-20 21:15:30 -0400

    Add files via upload

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Day 2: Operators
+            double mealCost = Convert.ToDouble(Console.ReadLine());
+            int tipPercent = Convert.ToInt32(Console.ReadLine());
+            int taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            double tip = mealCost * ((double)tipPercent / 100);
+            double tax = mealCost * ((double)taxPercent / 100);
+
+            double totalCost = Math.Round(mealCost + tip + tax);
+
+            Console.WriteLine("The total meal cost is " + totalCost + " dollars.");
+
+            Console.ReadLine();
+        }
+    }
+}
